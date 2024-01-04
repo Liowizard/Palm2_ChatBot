8245925c568832303d422f0b003d8d092efb74ca
Liowizard <ajeshrandam@gmail.com>
2024-01-04 16:41:06 +0530

    last

--- Chat_Bot.py
+++ Chat_Bot.py
@@ -5,54 +5,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# import google.generativeai as palm
 
 palm.configure(api_key=os.environ["PALM_API_KEY"])
 
-# Set up the model
-generation_config = {
-    "temperature": 0.9,
-    "top_p": 1,
-    "top_k": 1,
-    "max_output_tokens": 2048,
+
+defaults = {
+    "model": "models/gemini-pro",
+    "temperature": 0.4,
+    "candidate_count": 1,
+    "top_k": 40,
+    "top_p": 0.95,
+    "max_output_tokens": 1024,
 }
-
-safety_settings = [
-    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
-    },
-]
-
-model = palm.GenerativeModel(
-    model_name="gemini-pro",
-    generation_config=generation_config,
-    safety_settings=safety_settings,
-)
-
-conversation_history = []
 
 
 def chat_bot(input_msg):
-    global conversation_history
-    print("conversation_history", conversation_history)
+    response = palm.chat(messages=input_msg)
 
-    prompt = " ".join(conversation_history) + " " + input_msg
-    response = model.generate_content(prompt)
-
-    conversation_history.append(input_msg)
-    conversation_history.append(response.text)
-
-    # print(response.text)
-    return response.text
-
-
-# while True:
-#     st = input(">")
-#     print(chat_bot(st))
+    # response = response.reply(input_msg)
+    if not response.last:
+        response = palm.generate_text(
+            prompt=input_msg, temperature=0.8, candidate_count=1
+        )
+        print("Working")
+        if response.candidates:
+            return response.candidates[0]["output"]
+    else:
+        return response.last
